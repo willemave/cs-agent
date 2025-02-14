03045a132a614817932d2a70609a68d03c2de04b
Willem Ave <willem.ave@gmail.com>
2025-02-13 20:25:34 -0800

    Update layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,25 +18,9 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
                 }
             >
                 <div className={"flex"}>
-                    <Link href={"/"} prefetch={true}>
-                        <ElevenLabsLogo
-                            className={"h-[15px] w-auto hover:text-gray-500"}
-                        />
-                    </Link>
                 </div>
 
                 <div className={"flex gap-4 justify-end"}>
-                    <Link
-                        href="https://github.com/jonatanvm/convai-demo"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={"py-0.5"}
-                        aria-label="View source on GitHub"
-                    >
-                        <GithubLogo
-                            className={"w-5 h-5 hover:text-gray-500 text-[#24292f]"}
-                        />
-                    </Link>
                 </div>
             </nav>
             {children}
